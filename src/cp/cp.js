c8d1e4db830b92d6057a0d5271b2ac96f28e2168
nikitausaty <nikitausatiy@yandex.ru>
2022-12-03 17:55:03 +0300

    finish task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'node:child_process'
 
-spawnChildProcess();
+const spawnChildProcess = async (args) => {
+  const child = spawn('node', ['./src/cp/files/script.js', ...args])
+
+  process.stdin.pipe(child.stdin)
+
+  child.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+}
+
+spawnChildProcess(['--one', '--two', '--three'])
